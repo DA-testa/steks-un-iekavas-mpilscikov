e0a4431be06536051b97547ce5168161b9423240
raganubeka <m.pilscikov@gmail.com>
2023-02-17 14:29:31 +0200

    added unclosed brackets

--- main.py
+++ main.py
@@ -13,12 +13,16 @@ def find_mismatch(text):
     opening_brackets_stack = []
     for i, next in enumerate(text):
         if next in "([{":
-            opening_brackets_stack.append(next)
+            opening_brackets_stack.append(Bracket(next, i))
 
         if next in ")]}":
-            last_opening = opening_brackets_stack.pop()
+            last_opening = opening_brackets_stack.pop().char
             if not are_matching(last_opening, next):
                 return i + 1
+
+    else:
+        if opening_brackets_stack:
+            return opening_brackets_stack[0].position
 
     return -1
             
